1f294be8c5a709a3a92be6856af3bf07bd6fa91d
BorjaEst <boressan@outlook.com>
2020-03-18 09:05:27 +0100

    Added ndarray functions size and shape

--- src/ndarray.erl
+++ src/ndarray.erl
@@ -1,11 +1,11 @@
 %%%-------------------------------------------------------------------
-%%% File    : example_SUITE.erl
 %%% Author  :
 %%% Description :
 %%%
 %%% Created :
 %%%-------------------------------------------------------------------
 -module(ndarray).
+-compile({no_auto_import, [size/1]}).
 -compile([export_all, nowarn_export_all]). %% TODO: To delete after build
 
 -include_lib("kernel/include/logger.hrl").
@@ -33,12 +33,20 @@
 %% @doc Creates a multidimensional array from a list of lists.
 %% @end
 %%--------------------------------------------------------------------
--spec new(Shape :: shape(), Buffer :: buffer()) ->
+-spec new(Shape :: shape(), Array :: array:array()) ->
 	NdArray :: ndarray().
-new(Shape, Buffer) ->
+new(Shape, Array) ->
+  Buffer = array:resize(ltools:mult(Shape), Array),
 	#ndarray{
     shape = Shape,
-    buffer = Buffer}. %TODO, set the size of the buffer to the defined on shape 
+    buffer = Buffer}. 
+
+new_test() ->
+  Array = [[[1,2],[3,4]],[[5,6],[7,8]]],
+  Shape = [2,2,2],
+  Buffer = array:from_list(list:flatten(Array)), 
+  NdArray = new(Shape, Buffer), 
+  ?assertEqual(8, size(NdArray)).
 
 %%--------------------------------------------------------------------
 %% @doc Returns the ndarray shape.
@@ -49,5 +57,12 @@ new(Shape, Buffer) ->
 shape(NdArray) -> 
   NdArray#ndarray.shape.
 
-
+%%--------------------------------------------------------------------
+%% @doc Returns the ndarray size.
+%% @end
+%%--------------------------------------------------------------------
+-spec size(NdArray :: ndarray()) ->
+  Size :: integer().
+size(NdArray) -> 
+  array:size(NdArray#ndarray.buffer).
 
