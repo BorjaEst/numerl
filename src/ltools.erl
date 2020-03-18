6ad4421c3fcd9e501a406d7bc6bf42ea6a71b237
BorjaEst <boressan@outlook.com>
2020-03-18 17:32:32 +0100

    New function each N elements

--- src/ltools.erl
+++ src/ltools.erl
@@ -23,7 +23,7 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec sum(List :: [number()]) -> 
-    number().
+    Result :: number().
 sum(List) -> 
     lists:sum(List).
 
@@ -36,7 +36,7 @@ sum_test() ->
 %% @end
 %%--------------------------------------------------------------------
 -spec mult(List :: [number()]) -> 
-    number().
+    Result :: number().
 mult([H | Rest]) -> 
     H * mult(Rest);
 mult([]) -> 
@@ -46,7 +46,42 @@ mult_test() ->
     ?assertEqual(24, mult([1, 2, 3, 4])),
     ?assertEqual(-2.0, mult([1, 1.0, 2, -1])).
 
+%%--------------------------------------------------------------------
+%% @doc Drops the nth element of a list.
+%% @end
+%%--------------------------------------------------------------------
+-spec drop(N :: integer(), List1 :: [term()]) -> 
+    List2 :: [term()].
+drop(N, [A,B,C,D|Xs]) when N >= 4 -> [A,B,C,D | drop(N-4, Xs)];
+drop(3, [A,B,C,_|Xs])             -> [A,B,C|Xs];
+drop(2, [A,B,_  |Xs])             -> [A,B  |Xs];
+drop(1, [A,_    |Xs])             -> [A    |Xs];
+drop(0, [_      |Xs])             ->        Xs .
 
+drop_test() -> 
+    Seq9 = lists:seq(0,9),
+    ?assertEqual(tl(Seq9), drop(0, Seq9)),
+    ?assertEqual([0,1,3,4,5,6,7,8,9], drop(2, Seq9)),
+    ?assertEqual([0,1,2,3,4,5,7,8,9], drop(6, Seq9)),
+    ?assertEqual(lists:droplast(Seq9), drop(length(Seq9)-1, Seq9)),
+    ?assertError(function_clause, drop(length(Seq9), Seq9)).
 
+%%--------------------------------------------------------------------
+%% @doc Creates a sublist of every nth elements.
+%% @end
+%%--------------------------------------------------------------------
+-spec each(N :: integer(), List1 :: [term()], Start :: integer()) -> 
+    List2 :: [term()].
+each(N, [_ | Rest], I) when I >= 1 -> each(N, Rest, I-1);
+each(N, [H | Rest], 0) -> [H | each(N, Rest, N-1)];
+each(_, [], _) -> [].
 
+each_test() -> 
+    Seq9 = lists:seq(0,9),
+    ?assertEqual(Seq9, each(1, Seq9, 0)),
+    ?assertEqual(tl(Seq9), each(1, Seq9, 1)),
+    ?assertEqual([0,2,4,6,8], each(2, Seq9, 0)),
+    ?assertEqual([1,3,5,7,9], each(2, Seq9, 1)),
+    ?assertEqual([2,4,6,8], each(2, Seq9, 2)),
+    ?assertEqual([2,5,8], each(3, Seq9, 2)).
 
