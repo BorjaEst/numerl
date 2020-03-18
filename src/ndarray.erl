7c7f53aea192d8240ba6428b7d014fe47955a61f
BorjaEst <boressan@outlook.com>
2020-03-18 11:36:27 +0100

    reshape function added

--- src/ndarray.erl
+++ src/ndarray.erl
@@ -69,3 +69,49 @@ shape(NdArray) ->
 size(NdArray) -> 
   array:size(NdArray#ndarray.buffer).
 
+%%--------------------------------------------------------------------
+%% @doc Reshapes an array. 
+%% One shape dimension can be -1. In this case, the value is inferred 
+%% from the length of the array and remaining dimensions. 
+%% @end
+%%--------------------------------------------------------------------
+-spec reshape(NdArray :: ndarray(), Shape :: shape()) ->
+  NdArray :: ndarray().
+reshape(NdArray, Shape) ->
+  NewShape = calc_shape(Shape, size(NdArray), []),
+  NdArray#ndarray{shape = NewShape}.
+
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+calc_shape([N | Rest], Size, Acc) when N > 0-> 
+  calc_shape(Rest, Size div N, [N | Acc]);
+calc_shape([-1 | Rest], Size, Acc) ->
+  case Size div ltools:mult(Rest) of
+    N when N > 1 ->  calc_shape(Rest, Size div N, [N | Acc]);
+    _ -> error(badarg) 
+  end; 
+calc_shape([], 1, Acc) -> 
+  lists:reverse(Acc);
+calc_shape(_, _, _) -> 
+  error(badarg).
+
+calc_shape_test() -> 
+  % If shape has all elements (no -1) must return the same shape
+  Shape1 = [1, 2, 3],
+  Size = ltools:mult(Shape1),
+  ?assertEqual(Shape1, calc_shape(Shape1, Size, [])), 
+  % If there is a -1, the remaining size must be placed in that dim
+  Shape2 = [1, -1, 3],
+  ?assertEqual(Shape1, calc_shape(Shape2, Size, [])),
+  % A shape with more than one (-1) must rise a badarg error
+  ?assertError(badarg, calc_shape([1, -1, -1], Size, [])),
+  % A shape that does not fit with the size must return an error 
+  ?assertError(badarg, calc_shape([2, 1, 1], Size, [])),
+  ?assertError(badarg, calc_shape([2, 3, 3], Size, [])),
+  ok.
+
+
