fcce23cb73f909132d34d20d509c1b43519290fb
BorjaEst <boressan@outlook.com>
2020-03-21 22:48:10 +0100

    New function apply to run a fun over an specified axis

--- src/ndarray.erl
+++ src/ndarray.erl
@@ -5,9 +5,10 @@
 %%% Created :
 %%%-------------------------------------------------------------------
 -module(ndarray).
--compile({no_auto_import, [size/1]}).
+-compile({no_auto_import, [size/1, apply/3]}).
 -compile([export_all, nowarn_export_all]). %% TODO: To delete after build
 
+-include_lib("axis.hrl").
 -include_lib("kernel/include/logger.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -24,6 +25,8 @@
   buffer :: buffer()
 }).
 -type ndarray() :: #ndarray{}.
+
+-define(REVERS(List), lists:reverse(List)).
 
 
 %%%===================================================================
@@ -95,6 +98,7 @@ reshape(NdArray, Shape) ->
 %% An 'integer()' to specify a unique position; A ['integer()'] to 
 %% specify more than one position in that dimension; The atom ':' to
 %% indicate all the positions from that dimension.  
+%% @todo Use another atom (for ie';') to indicate all but reversed.
 %% @end
 %%--------------------------------------------------------------------
 -spec get(Indexes :: [index()], NdArray :: ndarray()) ->
@@ -134,6 +138,56 @@ get_3D_test() ->
                  get([':',[0,2],  0], Array3D)),
     ?assertEqual(new([  4,    1,  2], [ 5, 6, 7, 8,17,18,19,20]), 
                  get([':',  [1],':'], Array3D)).
+
+%%--------------------------------------------------------------------
+%% @doc Applies a function over the specified axis. The function must
+%% take as input a list of elements and return a number.
+%%  - fun(List :: [number()]) -> Result :: number().
+%% @end
+%%--------------------------------------------------------------------
+-spec apply(Fun :: function(), NdArray :: ndarray(), 
+            Axis :: axis()) ->
+  NdArray :: ndarray().
+apply(Fun, NdArray, Axis) ->
+  Shape = shape(NdArray),
+  Buffer = buffer(NdArray),
+  new(ltools:setnth(Axis+1, Shape, 1),
+      lists:map(Fun, vect_buffer(Axis, Shape, Buffer))).
+
+apply_3D_test() -> 
+  Array3D = new([4,3,2], lists:seq(1,24)), 
+  SumAll = fun lists:sum/1,
+  % Check the values on axis=0 are sum
+  ?assertEqual(new([1,3,2], lists:seq(10,90,16)), 
+               apply(SumAll, Array3D, ?AXIS_0)),
+  % Check the values on axis=1 are sum
+  ?assertEqual(new([4,1,2], [15,18,21,24,51,54,57,60]), 
+               apply(SumAll, Array3D, ?AXIS_1)),
+  % Check the values on axis=2 are sum
+  ?assertEqual(new([4,3,1], lists:seq(14,36, 2)), 
+               apply(SumAll, Array3D, ?AXIS_2)).
+
+apply_4D_test() -> 
+  Array4D = new([5,4,3,2], lists:seq(1,5*4*3*2)), 
+  SumAll = fun lists:sum/1,
+  % Check the values on axis=0 are sum
+  ?assertEqual(new([1,4,3,2], lists:seq(15,590,25)), 
+               apply(SumAll, Array4D, ?AXIS_0)),
+  % Check the values on axis=1 are sum
+  ?assertEqual(new([5,1,3,2], [34,38,42,46,50,114,118,122,126,130,
+                               194,198,202,206,210,274,278,282,286,
+                               290,354,358,362,366,370,434,438,442,
+                               446,450]), 
+               apply(SumAll, Array4D, ?AXIS_1)),
+  % Check the values on axis=2 are sum
+  ?assertEqual(new([5,4,1,2], [63,66,69,72,75,78,81,84,87,90,93,96,
+                               99,102,105,108,111,114,117,120,243,246,
+                               249,252,255,258,261,264,267,270,273,
+                               276,279,282,285,288,291,294,297,300]), 
+               apply(SumAll, Array4D, ?AXIS_2)),
+  % Check the values on axis=3 are sum
+  ?assertEqual(new([5,4,3,1], lists:seq(62,180,2)), 
+               apply(SumAll, Array4D, ?AXIS_3)).
 
 %%--------------------------------------------------------------------
 %% @doc Deletes all dimensions with shape length == 1. 
@@ -230,3 +284,45 @@ buffer_index_test() ->
                  buffer_index([':',[0,2],  0], Dim)),
     ?assertEqual([[[ 5, 6, 7, 8]],[[17,18,19,20]]], 
                  buffer_index([':',  [1],':'], Dim)).
+
+%%--------------------------------------------------------------------
+vect_buffer(Axis, Shape, Buffer) -> 
+    {DH, _} = lists:split(Axis+1, [1 | Shape]),
+    DBunch = ltools:mult(DH),
+    DAxis = lists:nth(Axis+1, Shape),
+    fill_bunch(DBunch, DBunch*DAxis, Buffer).
+
+fill_bunch(_DBunch,_Size,     []) -> [];
+fill_bunch( DBunch, Size, Buffer) -> 
+    Lxx = [[] || _ <- lists:seq(1, DBunch)],
+    {Vx , Rest} = lists:split(Size, Buffer),
+    fill_bunch(Vx, {Lxx, []}) ++ fill_bunch(DBunch, Size, Rest).
+
+fill_bunch([V | Vx], {[   Lx |Lxx],         Axx }) -> 
+    fill_bunch( Vx , {        Lxx , [[V|Lx]|Axx]});
+fill_bunch(     Vx , {         [] ,         Axx }) -> 
+    fill_bunch( Vx , {?REVERS(Axx),          [] });
+fill_bunch(     [] , {        Axx ,          [] }) -> 
+    Axx.
+
+vect_buffer3D_test() -> 
+    Shape = [4,3,2],
+    Buffer = lists:seq(1,24),
+    ?assertEqual([[ 4, 3, 2, 1],[ 8, 7, 6, 5],[12,11,10, 9],
+                  [16,15,14,13],[20,19,18,17],[24,23,22,21]], 
+                 vect_buffer(0, Shape, Buffer)),
+    ?assertEqual([[ 9, 5, 1],[10, 6, 2],[11, 7, 3],[12, 8, 4],
+                  [21,17,13],[22,18,14],[23,19,15],[24,20,16]], 
+                 vect_buffer(1, Shape, Buffer)),
+    ?assertEqual([[13, 1],[14, 2],[15, 3],[16, 4],[17, 5],[18, 6],
+                  [19, 7],[20, 8],[21, 9],[22,10],[23,11],[24,12]], 
+                 vect_buffer(2, Shape, Buffer)).
+
+vect_buffer4D_test() -> 
+    Shape = [5,4,3,2],
+    Buffer = lists:seq(1,ltools:mult(Shape)),
+    ?assertEqual([[X+Y
+                    ||X<-lists:seq(60, 0,-60)]
+                    ||Y<-lists:seq( 1,60,  1)], 
+                 vect_buffer(3, Shape, Buffer)).
+
