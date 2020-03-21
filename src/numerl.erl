dc10c2540190b61c007149cd6a10f635b8d8f555
BorjaEst <boressan@outlook.com>
2020-03-21 23:24:12 +0100

    Numerl module to perform math operations in ndarrays

--- src/numerl.erl
+++ src/numerl.erl
@@ -5,15 +5,13 @@
 %%% Created :
 %%%-------------------------------------------------------------------
 -module(numerl).
--compile([export_all, nowarn_export_all]). %% TODO: To delete after build
 
 -include_lib("axis.hrl").
 -include_lib("kernel/include/logger.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 %% API
-% -export([]).
-% -type_export([]).
+-export([mean/1, mean/2]).
 
 
 %%%===================================================================
@@ -21,47 +19,36 @@
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @doc 
+%% @doc Computes the arithmetic mean along the specified axis.
+%% Returns the average of the array elements. The average is taken 
+%% over the flattened array by default, otherwise over the specified
+%% axis. 
 %% @end
 %%--------------------------------------------------------------------
 -spec mean(NdArray :: ndarray:ndarray()) -> 
     Mean :: float().
 mean(NdArray) -> 
-    ok.
+    ltools:mean(ndarray:buffer(NdArray)).
 
 -spec mean(NdArray :: ndarray:ndarray(), Axis :: integer()) -> 
     NdArray :: ndarray:ndarray().
-mean(NdArray, 0 = Axis) -> 
-
-
-    Shape = ltools:drop(Axis, ndarray:shape(NdArray)),
-    Buffer = todo,
-    ndarray:new(Shape, Buffer).
-
+mean(NdArray, Axis) -> 
+    MeanF = fun ltools:mean/1,
+    ndarray:apply(MeanF, NdArray, Axis).
 
 mean_test() -> 
-    Buffer = array:from_list([2,4,2,4,4,4,2,2]),
-    Shape = [4, 2],
-    NdArray = ndarray:new(Shape, Buffer),
+    NdArray = ndarray:new([4,2], [2,4,2,4,4,4,2,2]),
+    % Mean over flattened array
     ?assertEqual(3.0, mean(NdArray)),
-    ?assertEqual(array:from_list([12.0, 12.0]), 
-                 ndarray:buffer(mean(NdArray, ?AXIS_0))),
-    ?assertEqual(array:from_list([3.0, 4.0, 2.0, 3.0]), 
-                 ndarray:buffer(mean(NdArray, ?AXIS_1))),
-    ?assertError(badarg, mean(NdArray, ?AXIS_2)),
-
-    % Buffer = array:from_list([1,2,3,1,2,3,1,2,3]),
-    % Shape = [array:size(Buffer)],
-    % NdArray = ndarray:new(Shape, Buffer),
-    % % Averaging over every n elements of a ndarray
-    % AxisMean = mean(ndarray:reshape(NdArray, [-1, 3]), ?AXIS_1),
-    % ?assertEqual([2,2,2], ndarray:buffer(AxisMean)).
-
-    ok.
-
-
-
-
-
-
+    % Mean over an specified axis
+    ?assertEqual(ndarray:new([1,2], [3.0, 3.0]), 
+                 mean(NdArray, ?AXIS_0)),
+    ?assertEqual(ndarray:new([4,1], [3.0,4.0,2.0,3.0]), 
+                 mean(NdArray, ?AXIS_1)),
+    % Averaging over every n elements of a vector 
+    Vector = ndarray:new([9], [1,2,3,1,2,3,1,2,3]),
+    ?assertEqual(ndarray:new([1,3], [2.0,2.0,2.0]),
+                 mean(ndarray:reshape(Vector, [-1,3]), ?AXIS_0)),
+    ?assertEqual(ndarray:new([3,1], [1.0,2.0,3.0]),
+                 mean(ndarray:reshape(Vector, [-1,3]), ?AXIS_1)).
 
